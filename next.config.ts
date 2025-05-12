7a43150a295424dcb3e6038c08a60a987344fb7e
rensly <rensly521@163.com>
2025-05-12 17:40:15 +0800

    fix

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      bcrypt: 'bcryptjs'
-    };
-    return config;
-  }
 };
 
 export default nextConfig;
